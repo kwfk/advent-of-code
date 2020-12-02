8d51a0ff05f804830330b3729ca9d98df31ffd67
Frederick Kim <freddy.kim8@gmail.com>
2020-12-02 14:31:40 -0800

    finish day 2

---
+++ day2/sol.py
@@ -0,0 +1,26 @@
+fs = open('day2/input.txt', 'r')
+
+count = 0
+
+while True:
+    line = fs.readline()
+    if not line:
+        break
+
+    args = line.split(' ')
+    cond = args[0].split('-')
+    letter = args[1][0]
+    password = args[2]
+    
+    # check = password.count(letter)
+    # if check <= int(cond[1]) and check >= int(cond[0]):
+    #     count += 1
+
+    one = password[int(cond[0]) - 1] == letter
+    two = password[int(cond[1]) - 1] == letter
+    if one ^ two:
+        count += 1
+
+print(count)
+
+fs.close()
