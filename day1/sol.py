ee3594b75138cc9aa3d049e66a840d5b1b2de00a
Frederick Kim <freddy.kim8@gmail.com>
2020-12-01 18:00:36 -0800

    finish day 1

---
+++ day1/sol.py
@@ -0,0 +1,12 @@
+fs = open('day1/input.txt', 'r')
+nums = fs.readlines()
+for i in range(len(nums) - 1):
+    for j in range(i, len(nums)):
+        for k in range(j, len(nums)):
+            one = int(nums[i])
+            two = int(nums[j])
+            thr = int(nums[k])
+            sum = one + two + thr
+            if sum == 2020:
+                print(one * two * thr)
+fs.close()
